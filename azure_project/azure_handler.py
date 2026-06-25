4bfda906821ae1990d3cb8ab9abc75a484cd425c
Lucariello Armando <armando.lucariello@gewiss.com>
2026-06-25 12:40:08 +0200

    first commit

---
+++ azure_project/azure_handler.py
@@ -0,0 +1,10 @@
+from azure.devops.connection import Connection
+from azure.devops.v7_0.core import CoreClient
+from azure.devops.v7_0.work_item_tracking.models import Wiql
+from msrest.authentication import BasicAuthentication
+from azure.devops.v7_0.test.test_client import TestClient
+from azure.devops.v7_0.test_plan.test_plan_client import TestPlanClient
+from azure.devops.v7_0.test_plan.models import *
+
+class AzureHandler:
+    pass
---
+++ export/excel_handler.py
@@ -0,0 +1,4 @@
+from openpyxl import Workbook
+from openpyxl.styles import Border, Side
+from openpyxl.styles import Font
+from openpyxl.styles import PatternFill

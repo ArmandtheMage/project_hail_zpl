5a1d4631ddb129de1e9e81251f2d2e392e124ac7
armando.lucariello <armandthemagic@gmail.com>
2026-07-14 16:59:59 +0200

    [feat] add set log level in releases

--- run.py
+++ run.py
@@ -1,10 +1,24 @@
+import sys
+import os
+import logging
 from utils.ZPL_log import ZPLLogger
 from gui.gui import GUI
 from azure_project.azure_handler import AzureHandler
 from export.excel_handler import ExcelHandler
 
 if __name__ == "__main__":
-    log = ZPLLogger("ZPLLogger")
+    if getattr(sys, 'frozen', False):
+        # Running as built executable
+        exe_name = os.path.basename(sys.executable).lower()
+        if "debug" in exe_name or "--debug" in sys.argv:
+            log_level = logging.DEBUG
+        else:
+            log_level = logging.INFO
+    else:
+        # Running locally from source
+        log_level = logging.DEBUG
+
+    log = ZPLLogger("ZPLLogger", log_level=log_level)
     az = AzureHandler(logger=log)
     eh = ExcelHandler(logger=log)
     gui = GUI(logger=log, azure_handler=az, excel_handler=eh)
